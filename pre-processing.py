c670686d4b5430da1fa73f19cb5817c67cc37106
Victor Leung <victorleungtw@gmail.com>
2016-11-26 14:43:21 +0800

    Clean up preprocessing

--- pre-processing.py
+++ pre-processing.py
@@ -16,8 +16,8 @@ dfSP500['Date'] = pd.to_datetime(dfSP500['Date'])
 
 dfSP500 = dfSP500[['Date', 'Close']]
 
-# print(dfSP500)
-# plt.plot(dfSP500['Date'], dfSP500['Close'])
+print(dfSP500.describe())
+plt.plot(dfSP500['Date'], dfSP500['Close'])
 # pylab.show()
 
 minOrMax = peakdetect(dfSP500['Close'].as_matrix(), lookahead=100)[0]
@@ -31,35 +31,8 @@ for index, peak in enumerate(minOrMax[:-1]):
 	if delta > 0:
 		labels.append("bull")
 	else:
-		percentageDrop = (delta / lastPeakValue) * 100
-		# print("percentageDrop")
-		# print(percentageDrop)
-		if abs(percentageDrop) > 20.0:
-			labels.append("bear")
-			print(dfSP500['Date'][minOrMax[index ][0]])
+		labels.append("bear")
 
-print("Bull:")
-print(labels.count("bull"))
+print("Bull:", labels.count("bull"))
 
-print("Bear:")
-print(labels.count("bear"))
-
-# 17.450001
-# 16.98
-
-# for ticker in tickers:
-# 	stockDf = df.loc[df['ticker'] == ticker]
-# 	last200days = stockDf.tail(200)
-# 	first100days = last200days.head(100)
-# 	last100days = last200days.tail(100)
-# 	firstDayClose = last100days.head(1)['close']
-# 	lastDayClose = last100days.tail(1)['close']
-# 	delta = np.float32(lastDayClose) - np.float32(firstDayClose)
-# 	percentageChange = (delta / np.float32(lastDayClose)) * 100
-# 	if delta[0] > 0:
-# 		filename = folderPath + ticker + "-up" + ".csv"
-# 	else:
-# 		filename = folderPath + ticker + "-down" +".csv"
-# 	text_file = open(filename, "w")
-# 	df.to_csv(filename)
-# 	text_file.close()
+print("Bear:", labels.count("bear"))

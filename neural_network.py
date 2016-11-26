3a145d687584524dbc4f130b808c6aa6b7b1cf1b
Victor Leung <victorleungtw@gmail.com>
2016-11-26 23:54:47 +0800

    Save result

--- neural_network.py
+++ neural_network.py
@@ -15,69 +15,112 @@ from sklearn.metrics import accuracy_score
 startDay = '2001-01-01'
 endDay = '2010-12-31'
 
-# HSI
+# # HSI
 HSI = Share('^HSI') 
 filePathHSI = './data/hsi.csv'
-# historyHSI = HSI.get_historical(startDay, endDay)
-# dfHSI = pd.DataFrame(historyHSI)
-dfHSI = pd.read_csv(filePathHSI)
+historyHSI = HSI.get_historical(startDay, endDay)
+dfHSI = pd.DataFrame(historyHSI)
+# dfHSI = pd.read_csv(filePathHSI)
 dfHSI['Date'] = pd.to_datetime(dfHSI['Date'])
 dfHSI = dfHSI.sort_values(by='Date', ascending=False)
-# dfHSI.to_csv(filePathHSI)
+dfHSI.to_csv(filePathHSI)
 
-# print(dfHSI.describe())
-
-# SP500
+# # SP500
 SP500 = Share('^GSPC')
 filePathSP500 = './data/sp500.csv'
-# historySP500 = SP500.get_historical(startDay, endDay)
-# dfSP500 = pd.DataFrame(historySP500)
-dfSP500 = pd.read_csv(filePathSP500)
+historySP500 = SP500.get_historical(startDay, endDay)
+dfSP500 = pd.DataFrame(historySP500)
+# dfSP500 = pd.read_csv(filePathSP500)
 dfSP500['Date'] = pd.to_datetime(dfSP500['Date'])
 dfSP500 = dfSP500.sort_values(by='Date', ascending=False)
-# dfSP500.to_csv(filePathSP500)
+dfSP500 = dfSP500.rename(columns = {'Open':'OpenSP'})
+dfSP500 = dfSP500.rename(columns = {'Close':'CloseSP'})
+dfSP500.to_csv(filePathSP500)
 
-# print(dfSP500.describe())
-
-# Gold
+# # Gold
 filePathGold = './data/gold-price.csv'
-dfGold = pd.read_csv(filePathGold)
-dfGold['Date'] = pd.to_datetime(dfGold['Date'])
-dfGold = dfGold.sort_values(by='Date', ascending=False)
-mask = (dfGold['Date'] > startDay) & (dfGold['Date'] <= endDay)
-dfGold = dfGold.loc[mask]
+# dfGold = pd.read_csv(filePathGold)
+# dfGold['Date'] = pd.to_datetime(dfGold['Date'])
+# dfGold = dfGold.sort_values(by='Date', ascending=False)
+# mask = (dfGold['Date'] > startDay) & (dfGold['Date'] <= endDay)
+# dfGold = dfGold.loc[mask]
 
-print(dfGold.describe())
 
-# # Oil price
+# # # Oil price
 filePathOil = './data/oil-price.csv'
-dfOil = pd.read_csv(filePathOil)
-dfOil['DATE'] = pd.to_datetime(dfOil['DATE'])
-dfOil = dfOil.sort_values(by='DATE', ascending=False)
-dfOil = dfOil[dfOil['VALUE'] != "."]
+# dfOil = pd.read_csv(filePathOil)
+# dfOil['DATE'] = pd.to_datetime(dfOil['DATE'])
+# dfOil = dfOil.sort_values(by='DATE', ascending=False)
+# dfOil = dfOil[dfOil['VALUE'] != "."]
 
-# print(dfOil.describe())
+# # Merge by date
+# dfHSI.index = dfHSI['Date']
+# dfSP500.index = dfSP500['Date']
+# dfGold.index = dfGold['Date']
+# dfOil.index = dfOil['DATE']
+
+# # print("dfHSI.describe()")
+# # print(dfHSI.describe())
+
+# # print("dfSP500.describe()")
+# # print(dfSP500.describe())
+
+# # print("dfGold.describe()")
+# # print(dfGold.describe())
+
+# # print("dfOil.describe()")
+# # print(dfOil.describe())
+
+# dfHSI = dfHSI.merge(dfSP500, left_on = 'Date', right_on = 'Date', how = 'inner')
+# dfHSI = dfHSI.merge(dfGold, left_on = 'Date', right_on = "Date", how = "inner")
+# dfHSI = dfHSI.merge(dfOil, left_on = 'Date', right_on = "DATE", how = "inner")
+
+# print(dfHSI)
+
+sp500 = pd.read_csv(filePathSP500)
+gold = pd.read_csv(filePathGold)
+oil = pd.read_csv(filePathOil)
+hsi = pd.read_csv(filePathHSI)
+
+sp500['Date'] = pd.to_datetime(sp500['Date'])
+gold['Date'] = pd.to_datetime(gold['Date'])
+oil['DATE'] = pd.to_datetime(oil['DATE'])
+hsi['Date'] = pd.to_datetime(hsi['Date'])
+
+gold.index = gold['Date']
+oil.index = oil['DATE']
+hsi.index = hsi['Date']
+sp500.index = sp500['Date']
+
+hsi = hsi.merge(sp500, left_on = 'Date', right_on = 'Date', how = 'inner')
+hsi = hsi.merge(gold, left_on = 'Date', right_on = "Date", how = "inner")
+hsi = hsi.merge(oil, left_on = 'Date', right_on = "DATE", how = "inner")
+
+hsi.to_csv("./data/merged.csv")
+
+dfHSI = hsi
 
 dfHSI["label"] = np.where(dfHSI['Close'] > dfHSI['Open'], '+1', '-1')
 
 dfHSI['Close'] = dfHSI['Close'].apply(lambda x: np.log(x))  
+dfHSI['CloseSP'] = dfHSI['CloseSP'].apply(lambda x: np.log(x)) 
 
 dfHSI['feature1'] = dfHSI['Close'] > dfHSI['Close'].shift()
 dfHSI['feature2'] = dfHSI['Close'] > dfHSI['Close'].shift(2)
 
-dfHSI['feature3'] = dfSP500['Close'] > dfSP500['Close'].shift()
-dfHSI['feature4'] = dfSP500['Close'] > dfSP500['Close'].shift(2)
+dfHSI['feature3'] = dfHSI['CloseSP'] > dfHSI['CloseSP'].shift()
+dfHSI['feature4'] = dfHSI['CloseSP'] > dfHSI['CloseSP'].shift(2)
 
-dfHSI['feature5'] = dfGold['Value'] > dfGold['Value'].shift()
-dfHSI['feature6'] = dfGold['Value'] > dfGold['Value'].shift(2)
+dfHSI['feature5'] = dfHSI['GOLD'] > dfHSI['GOLD'].shift()
+dfHSI['feature6'] = dfHSI['GOLD'] > dfHSI['GOLD'].shift(2)
 
-dfHSI['feature7'] = dfOil['VALUE'] > dfOil['VALUE'].shift()
-dfHSI['feature8'] = dfOil['VALUE'] > dfOil['VALUE'].shift(2)
+dfHSI['feature7'] = dfHSI['OIL'] > dfHSI['OIL'].shift()
+dfHSI['feature8'] = dfHSI['OIL'] > dfHSI['OIL'].shift(2)
 
 # print(dfHSI)
 
 dfHSI = dfHSI.ix[2:]
-dfHSI = dfHSI.fillna(0)
+# dfHSI = dfHSI.fillna(0)
 
 numberOfRow = dfHSI.shape[0]
 numberOfTrain = int(round(numberOfRow * 0.8))
